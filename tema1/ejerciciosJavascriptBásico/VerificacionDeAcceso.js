df66b6a0208021b143c7e5d6efcc53add52e5884
XxSebasx <125697490+XxSebasx@users.noreply.github.com>
2024-09-25 19:16:14 +0200

    hecho

--- tema1/ejerciciosJavascriptBásico/VerificacionDeAcceso.js
+++ tema1/ejerciciosJavascriptBásico/VerificacionDeAcceso.js
@@ -1,16 +1,16 @@
 function verificarAcceso(usuario, edad, cuentaRegistrada) {
-    if(edad >= 18) {
-        if(cuentaRegistrada == usuario) {
-            alert("Acceso concedido");
-        }else{
-            alert("Registrate primero");
-        }
-    }else{
-        alert("Acceso denegado");
-    }
+  if(edad >= 18) {
+      if(cuentaRegistrada == usuario) {
+          alert("Acceso concedido");
+      }else{
+          alert("Registrate primero");
+      }
+  }else{
+      alert("Acceso denegado");
+  }
 }
 
-
+let cuentaRegistrada ="Sebastian";
 let usuario = prompt("Dime tu nombre de usuario");
 let edad = parseInt(prompt("Dime tu edad"));
-verificarAcceso(usuario, edad, );
+verificarAcceso(usuario, edad, cuentaRegistrada);
